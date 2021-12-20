681f86f76442c2510bc2aaf0af6e1a03e129505d
shimanta505 <sarkershimanta@gmail.com>
2021-12-20 19:24:22 +0600

    added queue and leetcode package

---
+++ src/saccha/ArrayBasedQueue.java
@@ -0,0 +1,58 @@
+package saccha;
+
+public class ArrayBasedQueue {
+
+    int head=-1;
+    int tail =-1;
+    private int[] queue;
+
+    public ArrayBasedQueue(){
+        queue = new int[10];
+    }
+    public ArrayBasedQueue(int size){
+        queue = new int[size];
+    }
+
+    public boolean isEmpty(){
+        return tail == -1;
+    }
+    public boolean isFull(){
+        return queue.length == tail+1;
+    }
+
+    public void enQueue(int data){
+        if(isEmpty()){
+            head =0;
+        }else if(isFull()){
+            int[] temp =queue;
+            queue = new int[queue.length*2];
+            for (int i = head; i < temp.length; i++) {
+                queue[i]=temp[i];
+            }head=0;
+        }
+        tail++;
+        queue[tail]=data;
+    }
+    public int deQueue(){
+        if (isEmpty()){
+            throw new ArithmeticException ("queue is empty");
+        }else if(queue.length/2 == tail-head+1){
+            int[] temp =queue;
+            queue = new int[queue.length/2];
+            int index =0;
+            for (int i = head; i <=tail; i++) {
+                queue[index]=temp[i];index++;
+            }
+            head=0;tail = queue.length-1;
+        }
+        int data = queue[head];head++;
+        return data;
+    }
+
+    public void printQueue(){
+        for (int i = head; i <= tail; i++) {
+            System.out.print(queue[i]+", ");
+        }
+        System.out.println();
+    }
+}
---
+++ src/saccha/LinkedQueue.java
@@ -0,0 +1,56 @@
+package saccha;
+
+public class LinkedQueue  {
+
+   private Node head;
+   private Node tail;
+   private int size = 0;
+
+
+
+    public void enQueue(int data) {
+        Node newNode = new Node(data,null);
+
+        if (head==null){
+            head = newNode;
+        }else{
+            tail.next = newNode;
+        }
+        tail = newNode;
+    }
+
+
+    public int deQueue() {
+        int output = head.data;
+        head = head.next;
+        size--;
+        System.out.println();
+        return output;
+    }
+
+
+    public int first() {
+        System.out.println();
+        return head.data;
+    }
+
+
+    public void printQueue() {
+        Node temp = head;
+
+        while (temp!=null){
+            System.out.print(temp.data+"  ");
+            temp=temp.next;
+        }
+    }
+
+    private class Node{
+        int data;
+        Node next;
+
+        public Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+}
