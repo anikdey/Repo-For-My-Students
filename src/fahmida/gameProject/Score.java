78f2d7a81c73fbdedd6a7c0f4bc20b285b9b0b1b
Fahmida <fahmidayesmin65@gmail.com>
2022-01-10 09:14:58 +0600

    Game board project

--- src/fahmida/gameProject/Score.java
+++ src/fahmida/gameProject/Score.java
@@ -1,4 +1,4 @@
-package fahmida.gameProject;
+package gameProject;
 
 public class Score {
 
@@ -13,7 +13,13 @@ public class Score {
     public String getName(){
         return name;
     }
+    public void setName(String name){
+        this.name = name;
+    }
     public int getScore(){
         return score;
+    }
+    public void setScore(int score){
+        this.score = score;
     }
 }
